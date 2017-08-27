65bab626319ff78984c9a87ce892629cc91dd490
Haris Themistocleous <haristhemistocleous@Hariss-MacBook-Pro.local>
2017-08-27 09:31:23 +0200

    first commit

---
+++ IPA4/Help.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IPA4
+{
+    public partial class Help : Form
+    {
+        public Help()
+        {
+            InitializeComponent();
+        }
+
+        private void rtxTextHelp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Help_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
